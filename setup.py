0d471de476303ac1972a242fc5ab981f73a7b1af
Qiang Zhou <zhouqiang.cl@gmail.com>
2022-11-17 09:49:07 +0800

    mint nft
    Signed-off-by: Qiang Zhou <zhouqiang.cl@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="red-py-sdk",
-    version="0.0.6",
+    version="0.0.7",
     author="Reddio",
     author_email="contact@reddio.com",
     description="https://github.com/reddio-com/red-py-sdk",
