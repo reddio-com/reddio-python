3fbe6d5b6db2b837e6623619c1abac558ffc3f86
Qiang Zhou <zhouqiang.cl@gmail.com>
2022-11-22 19:45:19 +0800

    add token ids
    Signed-off-by: Qiang Zhou <zhouqiang.cl@gmail.com>

--- redpysdk/__init__.py
+++ redpysdk/__init__.py
@@ -74,8 +74,11 @@ class Reddio(object):
             raise e
 
 
-    def get_orders(self, contract_address):
+    def get_orders(self, contract_address, token_ids):
         uri = '/v1/orders' + '?contract_address=' + str(contract_address)
+        if isinstance(token_ids, list):
+            token_ids = ','.join(str(i) for i in token_ids)
+            uri += '&token_ids=' + str(token_ids)
         url = self.endpoint + uri
         headers = {'Content-Type': 'application/json'}
         r = requests.get(url, headers = headers)
